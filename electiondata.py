6e1cfb62a8787be703343b6085c37a56f6daa3d9
Sarah Platte <platte@mit.edu>
2014-12-11 11:29:20 -0500

    Changed to count all votes of each person
    Changed files:
    electiondata.py
    SarahPlatte_Assignment1.py

--- electiondata.py
+++ electiondata.py
@@ -7,12 +7,19 @@ class ElectionResults:
         self.file = open(self.filename, 'r')
         self.all_lines = self.file.readlines()
 
-    def states(self):
-        all_names = []
-        for line in self.all_lines:
+    def obama(self):
+        total_votes = []
+        for line in self.all_lines[1:]:
             columns = line.split(',')
-            all_names.append(columns[1])
-        return all_names[1:]
+            total_votes.append(int(columns[3]))
+        return sum(total_votes)
 
-    def state_count(self):
-        return len(self.states())
+    def romney(self):
+        total_votes = []
+        for line in self.all_lines[1:]:
+            columns = line.split(',')
+            total_votes.append(int(columns[5]))
+        return sum(total_votes)
+
+
+ 
--- object-oriented.py
+++ object-oriented.py
@@ -1,1 +1,14 @@
-# Print out all the state names from the csv# Coded in the "object-oriented" stylefrom electiondata import ElectionResultsfilename = '2012_US_election_state.csv'results = ElectionResults(filename)results.load()print "Opened file:"state_names = results.states()for state in state_names:    print "  "+stateprint "done ("+str(results.state_count())+" lines)"
+# Print out all the state names from the csv
+# Coded in the "object-oriented" style
+from electiondata import ElectionResults
+
+filename = '2012_US_election_state.csv'
+results = ElectionResults(filename)
+results.load()
+print "Opened file:"
+
+obama_perc = results.obamaper()
+for perc in obama_perc:
+    print "  "+perc
+
+print "done ("+str(results.obamaper_count())+" lines)"
